a003d67a386cce4edaf0df11226a6bc5cdcb104a
Alisher Tog'ayev <38793933+Alisherka7@users.noreply.github.com>
2019-04-27 10:57:47 -0700

    Update alignment.py

--- alignment.py
+++ alignment.py
@@ -28,3 +28,28 @@ for i in range(dl):
     h1 = '*'*b1
     print(''.ljust(dl*4-1),h1.center(dl*2))
     b1 = b1-2
+
+#################################################################################################    
+#thickness = int(input()) #This must be an odd number
+#c = 'G'
+
+#Top Cone
+#for i in range(thickness):
+#    print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
+
+#Top Pillars
+#for i in range(thickness+1):
+#    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
+
+#Middle Belt
+#for i in range((thickness+1)//2):
+#   print((c*thickness*5).center(thickness*6))
+
+#Bottom Pillars
+#for i in range(thickness+1):
+#    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
+
+#Bottom Cone
+#for i in range(thickness):
+#    print(((c*(thickness-i-1)).rjust(thickness)+c+(c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))
+###############################################################################################################
